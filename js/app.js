f2ffce70850da0c403116efe747d4571e343d916
SandraOkoli <sandraokoli@hotmail.co.uk>
2017-10-09 13:47:03 +0100

    created active function

--- js/app.js
+++ js/app.js
@@ -3,44 +3,44 @@ $(setup);
 //initally set list items to null
 let $li = null;
 let $interval = null;
-console.log($li);
+let $timer = null;
+
 
 function setup() {
   $li = $('li');
   getRandom();
-}
 
+}
+//
 function getRandom(){
-  const li = $li[Math.floor(Math.random()*$li.length)];
-  console.log(li);
-  $interval = setInterval(2000);
-  console.log($interval);
+  const list = $li[Math.floor(Math.random()*$li.length)];
+  console.log(list);
+  // $interval = setInterval(2000);
+  // console.log($interval);
+
+  active(list);
 }
 
+function active(list){
+  const $mole = $(list).addClass('active');
+  console.log($mole);
 
-// 1) pickRandomHole
-// 2) addMoleToHole
-// 3) addClickToHoleWhereMoleIs
-// 3) removeMoleFromHole
-// 4) removeClickFromHoleWhereMoleWas
-//
-//   function setup() {
-//     $holes = $(‘.hole’);
-//     pickRandomHole();
-//   }
-//
-//   function getRandom() {
-//   const hole = $holes[Math.floor(Math.random()*$holes.length)];
-//
-//   }
-// function setup () {
+  setSpeed(list);
+}
+
+// function setSpeed(list){
+//   $interval = setInterval(2000);
+//   console.log()
+// }
+//create Loop
+
+
+// function active(){
+//   const div = $('div').addClass('active');
+//   console.log(div);
+// }
+
+// function interval(){
+//   const
 //
 // }
-//   const $lis =
-//
-//
-//   $('li').each(function(){
-//     var id =[Math.floor(Math.random()*$lis.length)];
-//   });
-//
-//
