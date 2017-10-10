0faa464ebc45a0a374869b8c6ef459328240da26
SandraOkoli <sandraokoli@hotmail.co.uk>
2017-10-10 11:22:14 +0100

    Set interval and reset game

--- js/app.js
+++ js/app.js
@@ -1,17 +1,40 @@
 $(setup);
 
-//initally set list items to null
+//Declare global variables
 let $li = null;
-let $interval = null;
-let $timer = null;
-const $startGame =('button');
-const $timerDisplay =('.div');
+let interval = null;
+let counter = 61;
+// let countDown = null;
 
-//Set
+
+//Create setup function
+// The statements in the setup() function
+// execute once when the program begins
 function setup() {
   $li = $('li');
+
+  $('.start-button').on('click', startGame);
+}
+function startGame() {
+  interval = setInterval(timer, 2000);
+}
+function timer(){
+  counter --;
+  $('.timer-display').html(counter);
+
+  if (counter===0){
+    clearInterval(interval);
+    reset();
+  }
   getRandom();
 }
+
+function reset() {
+  counter = 61;
+  $('.timer-display').html('0');
+  // $('.start-button').html('Play Again?');
+}
+
 //Get random list items
 function getRandom(){
   const randomList = $li[Math.floor(Math.random()*$li.length)];
@@ -28,50 +51,16 @@ function displayMole(randomList){
     $(randomList).off('click');
   },1500);
 }
-
+//Remove mole once mole is hit
 function whackMole() {
   $(this).removeClass('mole');
 
 }
-//Set Timeout
 
+// 8. If 'active' (li) circle is clicked before timeout, update score by 10
+// 9. If 'active' (li) circle is not clicked before timeout decrease score by 10
 
-//
-// function whackMole(){
-//
-// }
-//     //Remove Class
-//   / whackMole(showMole);
-// }
-// // Click on mole when show mole is active(red))
-// function whackMole(showMole) {
-//   $(showMole).one('click', () => {
-//     console.log('clicked');
-//     //If mole is clicked on then removeClass
-//     if (true)
-//      const removeMole= $(list).removeClass('active');{
-//     //If mole is not clicked on
-//      }else
-//
-//   });
-// }
+function updateScore(){
 
-
-
-//Function to start the game(Set interval, timer and on click)
-// function startGame() {
-//   $interval = setInterval(countdown, 2000);
-//   getRandom();
-//   $startGame.on('click');
-// }
-//
-// function countdown() {
-//   $timer--;
-//   $timerDisplay.html($timer);
-//   console.log($timer);
-//
-//   if ($timer <= 0) {
-//     clearInterval($interval);
-//     // gameOver();
-//   }
-// }
+}
+//intervalSetup
